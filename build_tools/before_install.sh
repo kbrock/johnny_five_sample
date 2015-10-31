cac7a1f4d3db1daf5cf69d198cc2bce9fa170fc0
Keenan Brock <keenan@thebrocks.net>
2015-10-31 00:37:57 -0400

    always run before install

--- build_tools/before_install.sh
+++ build_tools/before_install.sh
@@ -7,9 +7,6 @@
 echo "gem: --no-ri --no-rdoc --no-document" > ~/.gemrc
 
 if [[ -f ${TRAVIS_BUILD_DIR}/.skip-ci ]] ; then
-  echo "skipping before_install"
-  cat ${TRAVIS_BUILD_DIR}/.skip-ci
-
   # change into a directory with minimal environment
   # this will NOP the rest of the build
   cd build_tools
