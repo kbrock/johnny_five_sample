c3fe7214e5c35d4d216cbe8039c145aa1023dfd1
Keenan Brock <keenan@thebrocks.net>
2015-11-02 10:34:03 -0500

    comment config

--- build_tools/johnny_five.rb
+++ build_tools/johnny_five.rb
@@ -93,7 +93,6 @@ class JohnnyFive
     extend Forwardable
 
     def initialize(travis)
-      # Travis
       @travis = travis
       @shallow_rules = {}
       @shallow_dependencies = {}
@@ -108,7 +107,7 @@ class JohnnyFive
     attr_accessor :branches
 
     def_delegators :@travis, :pr?, :branch, :component, :files, :verbose, :list
-    def_delegators :@travis, :verbose=
+    def_delegators :@travis
 
     def deduce
       if pr?
@@ -136,7 +135,7 @@ class JohnnyFive
       files.detect { |fn| regexp.match(fn) }.tap { |fn| puts "triggered by #{fn}" if verbose && fn }
     end
 
-    # @return Array[String] files that are not covered by any rules
+    # @return Array[String] files that are not covered by any rules (used by --check)
     def not_covered
       all_files = Regexp.union(shallow_rules.values.flatten)
       files.select { |fn| !all_files.match(fn) }
@@ -270,13 +269,17 @@ if __FILE__ == $PROGRAM_NAME
   $stderr.sync = true
 
   JohnnyFive.config do |cfg|
-    # cfg.verbose = false
-    # only build master branch (and PRs)
+    # if this is a branch (not a PR) - then only run for master
+    # comment out if you want to run for all branches
     cfg.branches << "master"
+
+    # if the Gemfile (exact name) changes, then the controllers and models (and dependencies) need to run
     cfg.file "Gemfile",                        %w(controllers models), :exact => true
+    # if any ruby files in these 3 directories (and descendents) change, then controllers need to run
     cfg.file "app/{assets,controllers,views}", "controllers", :ext => ".rb"
     cfg.file "app/models",                     "models", :ext => ".rb"
     cfg.file "app/helpers",                    "controllers", :ext => ".rb"
+    # ignore anything in bin directory. explicitly state, we know about this file, just ignore it
     cfg.file "bin",                            :none, :ext => ""
     cfg.file "build_tools",                    :none, :ext => ""
     # TODO: except not currently supported
@@ -284,6 +287,7 @@ if __FILE__ == $PROGRAM_NAME
     cfg.file "public",                         "ui", :ext => ""
     cfg.file "vendor",                         "ui", :ext => ""
 
+    # the tests have changed, run the corresponding tests (but not necessarily the dendencies)
     cfg.test "test/{controllers,views}",       "controllers-spec", :ext => "_spec.rb"
     cfg.test "test/fixtures",                  "models", :ext => ""
     cfg.test "test/helpers",                   "controllers-spec", :ext => "_spec.rb"
@@ -292,10 +296,12 @@ if __FILE__ == $PROGRAM_NAME
     cfg.file "gems/one/test",                  "one-spec", :ext => ""
     cfg.test "test/test_helper.rb",            :all
 
+    # if the code changes, then run the dependendencies as well.
     cfg.trigger "controllers",                 "ui"
     cfg.trigger "models",                      "controllers"
     cfg.trigger "one",                         %w(controllers models)
 
+    # if the code changes, then run the corresponding specs
     cfg.trigger "controllers",                 "controllers-spec"
     cfg.trigger "models",                      "models-spec"
     cfg.trigger "one",                         "one-spec"
