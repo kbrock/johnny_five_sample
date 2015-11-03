07b191c8a45dfdc172e319c6acbc56478a61820f
Keenan Brock <keenan@thebrocks.net>
2015-11-02 20:45:19 -0500

    fix config to full qualify directories

--- build_tools/config_two.rb
+++ build_tools/config_two.rb
@@ -7,14 +7,14 @@ JohnnyFive.config do |matrix|
   # models: Gemfile, one
   matrix.suite %w(controllers models) do |cfg|
     cfg.file "Gemfile", :exact => true
-    cfg.file "config", :ext => ""
+    cfg.file "config/", :ext => ""
     cfg.trigger "one"
   end
 
   # models: app/models/**/*.rb, test/fixtures
   matrix.suite "models" do |cfg|
     cfg.file "app/models", :ext => ".rb"
-    cfg.file "db", :ext => ".rb"
+    cfg.file "db/", :ext => ".rb"
     cfg.file "test/fixtures", :ext => ""
     cfg.test "test/models", :ext => "_test.rb"
   end
@@ -24,25 +24,25 @@ JohnnyFive.config do |matrix|
     cfg.file "app/views", :ext => ".jbuilder"
     cfg.file "app/helpers", :ext => ".rb"
     cfg.trigger "models"
-    cfg.test "test/{controllers,views}", :ext => "_test.rb"
+    cfg.test "test/{controllers,views}/", :ext => "_test.rb"
     cfg.test "test/helpers", :ext => "_test.rb"
   end
 
   matrix.suite "ui" do |cfg|
-    cfg.file "app/assets", :ext => ""
-    cfg.file "config", :ext => ""
-    cfg.file "public", :ext => ""
-    cfg.file "vendor", :ext => ""
+    cfg.file "app/assets/", :ext => ""
+    cfg.file "config/", :ext => ""
+    cfg.file "public/", :ext => ""
+    cfg.file "vendor/", :ext => ""
     cfg.trigger "controller"
-    cfg.test "test/integration", :ext => "_test.rb"
+    cfg.test "test/integration/", :ext => "_test.rb"
   end
 
   matrix.suite "j5" do |cfg|
-    cfg.file "build_tools", :ext => ""
+    cfg.file "build_tools/", :ext => ""
   end
 
   matrix.suite "one" do |cfg|
-    cfg.file "gems/one", :except => %r{gems/one/test}, :ext => ""
+    cfg.file "gems/one/", :except => %r{gems/one/test}, :ext => ""
     cfg.test "gems/one/test", :ext => ""
   end
 
@@ -50,10 +50,10 @@ JohnnyFive.config do |matrix|
     cfg.file "README.md", :exact => true
     cfg.file "Rakefile", :exact => true
 
-    cfg.file "bin", :ext => ""
-    cfg.file "log", :ext => ""
+    cfg.file "bin/", :ext => ""
+    cfg.file "log/", :ext => ""
     cfg.file "lib/tasks", :ext => ""
-    cfg.file "tmp", :ext => ""
+    cfg.file "tmp/", :ext => ""
   end
 
   matrix.suite :all do |cfg|
